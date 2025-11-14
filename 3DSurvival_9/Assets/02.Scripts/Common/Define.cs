91b8fa0127baad172cd431a475fc17a5146cea74
UihwanLee <luh5063014@naver.com>
2025-11-14 19:22:51 +0900

    feat: Common Defin.cs / Enum.cs 스크립트 추가

---
+++ 3DSurvival_9/Assets/02.Scripts/Common/Define.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Define
+{
+    // 게임에서 사용할 상수값을 모아놓은 스크립트
+    // region에서 변수 추가만 하면 git 충돌 방지됨
+    // 상수값 명 표시할 때는 대문자
+
+    // public const float SPEED
+    // public readonly float SPEED
+
+    #region Player
+
+
+
+    #endregion
+
+    #region AI
+
+
+
+    #endregion
+}
---
+++ 3DSurvival_9/Assets/02.Scripts/Common/Enum.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Enum
+{
+    // 게임에서 사용할 Enum 모아놓은 스크립트
+    // region에서 변수 추가만 하면 git 충돌 방지됨
+    // 상수값 명 표시할 때는 대문자
+
+    // public const float SPEED
+    // public readonly float SPEED
+
+    #region Player
+    
+
+    #endregion
+
+    #region AI
+
+
+
+    #endregion
+}
