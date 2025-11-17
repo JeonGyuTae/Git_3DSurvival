aa87cf1cb21b6fe9bdd4f365bfb4981334bc4796
Hagill <youmu12589@gmail.com>
2025-11-17 18:52:29 +0900

    feat: 시간에 따른 배고픔, 목마름 수치 하락 구현

--- 3DSurvival_9/Assets/02.Scripts/Player/PlayerCondition.cs
+++ 3DSurvival_9/Assets/02.Scripts/Player/PlayerCondition.cs
@@ -17,6 +17,8 @@ public class PlayerCondition : MonoBehaviour, IDamageble
     Condition thirsty { get { return uiCondition.thirsty; } }
     Condition stamina { get { return uiCondition.stamina; } }
 
+    [SerializeField] private float noHungerHealthDecay;
+    [SerializeField] private float noThirstyHealthDecay;
     // 배고픔과 목마름이 0일 때 체력 소모
     // 플레이어가 공격등의 데미지를 입었을 때 표현할 데미지 이벤트 액션
     private void Awake()
@@ -26,8 +28,23 @@ public class PlayerCondition : MonoBehaviour, IDamageble
 
     void Update()
     {
+        hunger.Sub(hunger.passiveValue * Time.deltaTime);
+        thirsty.Sub(thirsty.passiveValue * Time.deltaTime);
         stamina.Add(stamina.passiveValue * Time.deltaTime);
 
+        // 배고픔이 0일 때 시간동안 체력 피해
+        if(hunger.curValue <= 0f)
+        {
+            health.Sub(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        // 목마름이 0일 때 시간동안 체력 피해
+        if(thirsty.curValue <= 0f)
+        {
+            health.Sub(noThirstyHealthDecay * Time.deltaTime);
+        }
+
+        // 달리기도중 스태미나가 0이되면 달리기 상태를 해제
         if (controller != null && controller.isRunning)
         {
             stamina.Sub(controller.useRunStamina * Time.deltaTime);
@@ -36,6 +53,8 @@ public class PlayerCondition : MonoBehaviour, IDamageble
                 controller.isRunningFalse();
             }
         }
+
+        // 체력이 0이 됐을 때
         if(health.curValue == 0f)
         {
             Die();
