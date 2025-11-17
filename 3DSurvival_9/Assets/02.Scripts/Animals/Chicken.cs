dc0a7c0c713eb82c3367bccb6370541c6c999609
UihwanLee <luh5063014@naver.com>
2025-11-17 15:15:54 +0900

    feat: Chicken IInteractable 상속 및 인터페이스 구현

--- 3DSurvival_9/Assets/02.Scripts/Animals/Chicken.cs
+++ 3DSurvival_9/Assets/02.Scripts/Animals/Chicken.cs
@@ -2,7 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chicken : MonoBehaviour
+/// <summary>
+/// Chicken 스크립트
+/// 닭에 대한 Interact를 처리하는 스크립트이다.
+/// </summary>
+public class Chicken : MonoBehaviour, IInteractable
 {
     [SerializeField] private AnimalData data;
+
+    public string GetInteractPrompt()
+    {
+        return data.animalName;
+    }
+
+    public void ShowInteractUI()
+    {
+
+    }
+
+    public void HideInteractUI()
+    {
+        
+    }
+
+    public void OnInteract()
+    {
+
+    }
 }
