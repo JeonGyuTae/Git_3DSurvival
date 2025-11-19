f02ab90f0c82363402330172c23baf46ea5c4453
UihwanLee <luh5063014@naver.com>
2025-11-19 12:59:08 +0900

    feat: AnimalSpawn에서 Animal 오브젝트를 전체적으로 관리하는 AnimalSpawnManager 구현

--- 3DSurvival_9/Assets/02.Scripts/Animals/Spawn/AnimalSpawnManager.cs
+++ 3DSurvival_9/Assets/02.Scripts/Animals/Spawn/AnimalSpawnManager.cs
@@ -2,17 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// AnimalSpawn을 관리하는 스크립트
+/// 
+/// Animal이 죽었을 때 일정 시간 지나면 리스폰하게 도와줌
+/// 
+/// </summary>
 public class AnimalSpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AnimalSpawnManager Instance { get; private set; }
+
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private AnimalSpawn[] animalSpawns;
+
+    private void Start()
     {
-        
+        // 자식 오브젝트에 있는 AnimalSpawn을 가져옴
+        animalSpawns = GetComponentsInChildren<AnimalSpawn>();
+    }
+
+    public void Spawn(string key)
+    {
+        foreach (var spawn in animalSpawns)
+        {
+            if (spawn.Key == key)
+            {
+                spawn.Spawn();
+                Debug.Log($"{key} 제거");
+
+                // 리스폰 해야함 -> 코루틴 사용 (개별?)
+
+                return;
+            }
+        }
+
+        Debug.Log($"{key} key 값에 해당하는 objectPool이 존재하지 않습니다.");
+    }
+
+    public void Release(string key, GameObject obj)
+    {
+        foreach(var spawn in animalSpawns)
+        {
+            if (spawn.Key == key)
+            {
+                spawn.Release(obj);
+                Debug.Log($"{key} 제거");
+
+                // 리스폰 해야함 -> 코루틴 사용 (개별?)
+
+                return;
+            }
+        }
+
+        Debug.Log($"{key} key 값에 해당하는 objectPool이 존재하지 않습니다.");
     }
 }
