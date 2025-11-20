35739aeaf201d947a4683ad48cbdfc0a7b2b6b07
JeonGyuTae <115717251+JeonGyuTae@users.noreply.github.com>
2025-11-20 11:48:59 +0900

    feat: 지도 구현, MapController 생성 및 Prefab화, DialogueManager 위치 변경
    UI_Map -> Manager

---
+++ 3DSurvival_9/Assets/02.Scripts/UI/MapController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject mMap;
+
+    void Awake()
+    {
+        if (map == null)
+        {
+            Debug.Log("Map이 연동되지 않음");
+        }
+
+        if (mMap == null)
+        {
+            Debug.Log("Map이 연동되지 않음");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            OnMap();
+        }
+    }
+
+    void OnMap()
+    {
+        // 현재 상태 반전_지도(토글)
+        bool isMapActive = map.activeSelf;
+        map.SetActive(!isMapActive);
+
+        // 현재 상태 반전_미니맵(토글)
+        bool isMMapActive = mMap.activeSelf;
+        mMap.SetActive(!isMMapActive);
+    }
+}
